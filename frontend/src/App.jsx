09c1f8807d479e9495d0d958a18bb66b291c8e17
Filip <fbudzynski56@gmail.com>
2023-12-09 23:04:45 +0100

    refactor views

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,25 @@
-import { useState, useEffect     } from "react" ;
+import { useState, useEffect } from "react";
 
 function App() {
-  const [data, setData] = useState([ ] );
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
-      console.log(import.meta.env.VITE_API_URL)	
-;
-      try 
-
-{
+      console.log(import.meta.env.VITE_API_URL);
+      try {
         const response = await fetch(`${import.meta.env.VITE_API_URL}contests`);
         if (!response.ok) {
           throw new Error("Network response was not ok");
         }
         const result = await response.json();
+
         console.log(result);
         setData(result);
-      } catch (error) 
-	{
+      } catch (error) {
         console.error("Error fetching data:", error);
       }
     }
-    
+
     fetchData();
   }, []);
 
