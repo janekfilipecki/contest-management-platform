cea6d9714ab20e2864d094f75d7410e2f4c4f5b6
Filip <fbudzynski56@gmail.com>
2024-01-01 18:45:04 +0100

    deploy script fixes

--- backend/deploy_backend.sh
+++ backend/deploy_backend.sh
@@ -3,7 +3,6 @@
 ssh -tt azureuser@20.215.200.116 <<EOF
   cd /home/azureuser/contest-platform/backend
   git checkout fb-cicd
-  git pull
   nohup pipenv run python3 manage.py runserver 0.0.0.0:8000 &
   exit
 EOF
