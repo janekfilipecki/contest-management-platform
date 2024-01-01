b32cfc63c5120a18752af0f167761e915c35aa48
Filip <fbudzynski56@gmail.com>
2024-01-01 23:27:26 +0100

    kill and restart gunicorn

--- backend/deploy_backend.sh
+++ backend/deploy_backend.sh
@@ -3,6 +3,7 @@
 ssh -tt azureuser@20.215.200.116 <<EOF
   cd /home/azureuser/contest-platform/backend
   git checkout fb-cicd
+  pkill -f gunicorn
   pipenv run gunicorn -c gunicorn_config.py backend.wsgi --daemon
   exit
 EOF
